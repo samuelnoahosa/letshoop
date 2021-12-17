340560e440c9195ecb77319d4c76a9bbd809603a
samuelnoahosa <samuel.noah@outlook.com>
2021-12-17 14:37:46 -0500

    Add project files.

---
+++ letshoop/letshoop/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using letshoop.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace letshoop.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
